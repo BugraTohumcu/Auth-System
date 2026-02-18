03523bc160fef69bddc656cc234cd26b8775fc09
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-02-18 21:17:51 +0300

    logging and synchronized block added for rate limiter

--- src/main/java/com/bugra/filter/LoginRateLimiter.java
+++ src/main/java/com/bugra/filter/LoginRateLimiter.java
@@ -7,7 +7,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -16,10 +19,12 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Component
 public class LoginRateLimiter extends OncePerRequestFilter {
 
+    private final Logger logger = LoggerFactory.getLogger(LoginRateLimiter.class);
     private final int MAX_REQUEST = 5;
-    private final long WINDOW_SIZE = Duration.ofSeconds(30).toMillis();
+    private final long WINDOW_SIZE = Duration.ofSeconds(20).toMillis();
 
     private final Map<String, RequestInfo> requestMap = new ConcurrentHashMap<>();
 
@@ -39,32 +44,38 @@ public class LoginRateLimiter extends OncePerRequestFilter {
                   )
             );
 
-            int count = requestInfo.getCount().incrementAndGet();
+            synchronized (requestInfo){
 
-            if(System.currentTimeMillis() - requestInfo.getWindowStart() >= WINDOW_SIZE){
-                requestInfo.getCount().set(0);
-                filterChain.doFilter(request,response);
-            }
+                long now = System.currentTimeMillis();
+                if(now - requestInfo.getWindowStart() >= WINDOW_SIZE){
+                    logger.debug("Rate limit window reset for IP: {}", ip);
+                    requestInfo.setWindowStart(now);
+                    requestInfo.getCount().set(1);
+                }else {
+                    requestInfo.getCount().incrementAndGet();
+                }
 
-            if(count > MAX_REQUEST){
-                ObjectMapper mapper = new ObjectMapper();
-                ResponsePattern<String> res = new ResponsePattern<>
-                        (
-                        HttpStatus.TOO_MANY_REQUESTS.toString(),
-                        null,
-                        false
-                        );
+                if(requestInfo.getCount().get() > MAX_REQUEST){
+                    logger.warn("Rate limit exceed for IP: {}",ip);
+                    ObjectMapper mapper = new ObjectMapper();
+                    ResponsePattern<String> res = new ResponsePattern<>
+                            (
+                                    HttpStatus.TOO_MANY_REQUESTS.name(),
+                                    null,
+                                    false
+                            );
 
-                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-                response.setContentType("application/json");
-                response.getWriter().write(mapper.writeValueAsString(res));
+                    response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                    response.setContentType("application/json");
+                    response.getWriter().write(mapper.writeValueAsString(res));
+                    return;
+                }
 
-                return;
             }
 
         }
 
-
+        filterChain.doFilter(request,response);
     }
 
 }
