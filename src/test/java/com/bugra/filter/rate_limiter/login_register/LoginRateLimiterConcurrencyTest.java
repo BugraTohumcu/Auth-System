7890d7d61fd40377b3af7dffa299c78229f5da5f
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-02-23 22:25:39 +0300

    feat(rate-limiter): add integration test (limit, window reset), concurrency helper and test

---
+++ src/test/java/com/bugra/filter/rate_limiter/login_register/LoginRateLimiterConcurrencyTest.java
@@ -0,0 +1,59 @@
+package com.bugra.filter.rate_limiter.login_register;
+
+import com.bugra.filter.helper.rate_limiter.LoginRateLimiterTestHelper;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class LoginRateLimiterConcurrencyTest {
+
+    private final Logger logger = LoggerFactory.getLogger(LoginRateLimiterConcurrencyTest.class);
+    private final int LIMIT = 5;
+
+    @Test
+    void shouldBeThreadSafe() throws InterruptedException {
+        String ip = "192.168.1.1";
+        LoginRateLimiterTestHelper limiter = new LoginRateLimiterTestHelper();
+
+        int threadSize = 100;
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch downLatch = new CountDownLatch(threadSize);
+
+        ExecutorService pool = Executors.newFixedThreadPool(threadSize);
+
+        AtomicInteger successCount = new AtomicInteger(0);
+
+        for(int i = 0; i< threadSize; i++){
+            pool.submit(() -> {
+                try {
+                    startLatch.await();
+
+                    if(limiter.tryConsume(ip)){
+                        successCount.incrementAndGet();
+                    }
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }finally {
+                    downLatch.countDown();
+                }
+            });
+        }
+
+        logger.info("All thread are ready to test");
+        startLatch.countDown();
+
+        downLatch.await();
+
+        assertEquals(LIMIT, successCount.get());
+    }
+}
---
+++ src/test/java/com/bugra/filter/rate_limiter/login_register/LoginRateLimiterIntegrationTest.java
@@ -0,0 +1,107 @@
+package com.bugra.filter.rate_limiter.login_register;
+
+import com.bugra.dto.ResponsePattern;
+import com.bugra.enums.EndPoints;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LoginRateLimiterIntegrationTest{
+
+    private final int LIMIT = 5;
+    private final long WINDOW_SIZE  = Duration.ofSeconds(20).toMillis();
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+
+
+    @Test
+    void shouldBlockRequestAfterLimit() throws Exception {
+        String ip = "192.168.1" + UUID.randomUUID();
+        String testUser = """
+                {
+                    "password":"Abc123",
+                    "email":"necmiK@g.com"
+                }
+                """;
+        for(int i = 0; i < 5; i++){
+            mvc.perform(post(EndPoints.LOGIN.getPath())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(testUser)
+                    .header("X-Forwarded-For", ip))
+                    .andExpect(status().isOk());
+        }
+
+        mvc.perform(post(EndPoints.LOGIN.getPath())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(testUser)
+                .header("X-Forwarded-For", ip))
+                .andExpect(status().isTooManyRequests())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(result -> {
+                    ResponsePattern<?> res = mapper.readValue(
+                        result.getResponse().getContentAsString(),
+                        ResponsePattern.class
+                    );
+
+                    assert !res.success();
+                    assert res.data() == null;
+                });
+    }
+
+
+    @Test
+    void shouldResetWindow() throws Exception {
+        String ip = "192.168.1." + UUID.randomUUID();
+        String testUser = """
+                {
+                    "password":"Abc123",
+                    "email":"necmiK@g.com"
+                }
+                """;
+        for(int i = 0; i < LIMIT ; i++){
+            mvc.perform(post(EndPoints.LOGIN.getPath())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(testUser)
+                    .header("X-Forwarded-For", ip))
+                    .andExpect(status().is(not(429)));
+        }
+
+        // This request going to trigger rate limiter
+        mvc.perform(post(EndPoints.LOGIN.getPath())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(testUser)
+                        .header("X-Forwarded-For", ip))
+                .andExpect(status().isTooManyRequests());
+
+
+        Thread.sleep(WINDOW_SIZE + 1000 );
+
+
+        mvc.perform(post(EndPoints.LOGIN.getPath())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(testUser)
+                        .header("X-Forwarded-For", ip))
+                .andExpect(status().is(not(429)));
+
+
+    }
+}
