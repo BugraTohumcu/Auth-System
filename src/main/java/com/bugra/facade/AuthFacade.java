a67492fcaedd6a0f2eb377a8ba371b06c3c2a03d
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-02-25 18:14:57 +0300

    refactor(auth-facade): change jwtService to refreshToken for handling refresh token save

--- src/main/java/com/bugra/facade/AuthFacade.java
+++ src/main/java/com/bugra/facade/AuthFacade.java
@@ -2,8 +2,10 @@ package com.bugra.facade;
 
 import com.bugra.model.User;
 import com.bugra.security.dto.TokenPayload;
+import com.bugra.service.CookieService;
 import com.bugra.service.JwtService;
 import com.bugra.enums.Token;
+import com.bugra.service.RefreshTokenService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +13,20 @@ import org.springframework.stereotype.Service;
 public class AuthFacade {
 
     private final JwtService jwtService;
+    private final CookieService cookieService;
+    private final RefreshTokenService refreshTokenService;
 
-    public AuthFacade(JwtService jwtService) {
+    public AuthFacade(JwtService jwtService, CookieService cookieService, RefreshTokenService refreshTokenService) {
         this.jwtService = jwtService;
+        this.cookieService = cookieService;
+        this.refreshTokenService = refreshTokenService;
     }
 
     public void saveTokenAndSetCookie(User user, HttpServletResponse response) {
         TokenPayload payload = TokenPayload.fromUser(user);
         String refreshToken = jwtService.createToken(Token.refresh_token, payload);
         String accessToken = jwtService.createToken(Token.access_token, payload);
-        jwtService.saveRefreshToken(refreshToken, user);
-        jwtService.setJwtCookies(accessToken, refreshToken, response);
+        refreshTokenService.save(refreshToken, user);
+        cookieService.setJwtCookies(accessToken, refreshToken, response);
     }
 }
