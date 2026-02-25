9d354c33aa89419d85a2207d84465b771ecd0977
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-02-25 18:32:47 +0300

    feat(exception): add token not found exception

---
+++ src/main/java/com/bugra/exceptions/TokenNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bugra.exceptions;
+
+public class TokenNotFoundException extends RuntimeException {
+    public TokenNotFoundException(String message) {
+        super(message);
+    }
+}
