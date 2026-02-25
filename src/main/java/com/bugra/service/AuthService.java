5c7fe95d777ecab703f79d3327694c1876780cde
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-02-25 18:17:52 +0300

    feat(auth-service): create for refresh token rotation

---
+++ src/main/java/com/bugra/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.bugra.service;
+
+import com.bugra.model.User;
+import com.bugra.security.dto.TokensRefreshed;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
+
+import static com.bugra.enums.Token.refresh_token;
+
+@Service
+public class AuthService {
+
+    private final RefreshTokenService refreshTokenService;
+    private final CookieService cookieService;
+
+    public AuthService(RefreshTokenService refreshTokenService, CookieService cookieService) {
+        this.refreshTokenService = refreshTokenService;
+        this.cookieService = cookieService;
+    }
+
+    public void refreshToken(User user, HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = cookieService.extractTokenFromCookies(refresh_token.toString(), request);
+        refreshTokenService.removeRefreshToken(refreshToken);
+        TokensRefreshed tokens = refreshTokenService.refreshTokens(user);
+        cookieService.setJwtCookies(tokens.access_token(), tokens.refresh_token(), response);
+    }
+}
