9394523fd22ddb59d975e3b8acf4289a480a8232
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-02-25 18:44:37 +0300

    refactor(jwt-service): remove unused imports

--- src/main/java/com/bugra/service/JwtService.java
+++ src/main/java/com/bugra/service/JwtService.java
@@ -1,19 +1,10 @@
 package com.bugra.service;
 
-import com.bugra.model.User;
 import com.bugra.security.JwtTokenProvider;
 import com.bugra.security.dto.TokenPayload;
-import com.bugra.security.dto.TokensRefreshed;
 import com.bugra.enums.Token;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
-
-import static com.bugra.enums.Token.access_token;
-import static com.bugra.enums.Token.refresh_token;
 
 @Service
 public class JwtService {
