7b0400278205e124b3ba6c086acb93fb43ead37d
Ahmet Buğra Tohumcu <tohumcuahmetbugra@gmail.com>
2026-02-26 18:30:07 +0300

    refactor(refresh-repo): remove unnecessary locking and packages

--- src/main/java/com/bugra/repo/RefreshTokenRepo.java
+++ src/main/java/com/bugra/repo/RefreshTokenRepo.java
@@ -1,24 +1,20 @@
 package com.bugra.repo;
 
 import com.bugra.model.RefreshToken;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RefreshTokenRepo extends JpaRepository<RefreshToken, String> {
 
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Modifying
-    @Query("Update RefreshToken t set t.revoked = true where t.jti = :jti and  t.revoked = false")
-    int revokeTokenByJti(@Param("jti") String jti);
 
+    @Modifying
+    @Query("Update RefreshToken t set t.revoked = true where t.jti = :jti and t.revoked = false")
+    int revokeTokenByJti(@Param("jti") String jti);
 
     @Query("Select t.revoked from RefreshToken t where t.jti = :jti")
     boolean isTokenRevoked(@Param("jti") String jti);
